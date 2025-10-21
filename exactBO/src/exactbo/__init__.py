99b21aeef03c7085d1b99b1b148594d8ce5b95ca
juanesfco <juan.florez@tamu.edu>
2025-10-21 13:36:22 -0500

    repo structure

---
+++ exactBO/src/exactbo/__init__.py
@@ -0,0 +1,27 @@
+"""
+exactBO: minimal exact Bayesian Optimization scaffold.
+
+Exports:
+- Box, split_box, corners
+- expected_improvement
+- ei_bounds_from_mu_sigma_intervals, EIBounds
+- PartitionMaxEISearch
+- ExactBOLoop, BOResult
+"""
+from .partition import Box, split_box, corners
+from .ei import expected_improvement
+from .ei_bounds import ei_bounds_from_mu_sigma_intervals, EIBounds
+from .loop_partition import PartitionMaxEISearch
+from .loop_exactbo import ExactBOLoop, BOResult
+
+__all__ = [
+    "Box",
+    "split_box",
+    "corners",
+    "expected_improvement",
+    "ei_bounds_from_mu_sigma_intervals",
+    "EIBounds",
+    "PartitionMaxEISearch",
+    "ExactBOLoop",
+    "BOResult",
+]
