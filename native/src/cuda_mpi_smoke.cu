718a0a1dd679be230cae45def00832851924909c
juanesfco <juan.florez@tamu.edu>
2026-06-26 17:46:35 -0500

    cuda mpi smoke test

--- native/src/cuda_mpi_smoke.cu
+++ native/src/cuda_mpi_smoke.cu
@@ -2,6 +2,11 @@
 #include <cstdlib>
 #include <cuda_runtime.h>
 #include <mpi.h>
+#include <unistd.h>
+
+#ifdef __linux__
+#include <sched.h>
+#endif
 
 #define CUDA_CHECK(call)                                                     \
     do {                                                                     \
@@ -20,6 +25,14 @@ __global__ void fill_rank(double* x, int n, int rank) {
     }
 }
 
+int current_cpu() {
+#ifdef __linux__
+    return sched_getcpu();
+#else
+    return -1;
+#endif
+}
+
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
@@ -27,6 +40,10 @@ int main(int argc, char** argv) {
     int size = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    char processor_name[MPI_MAX_PROCESSOR_NAME] = {};
+    int name_len = 0;
+    MPI_Get_processor_name(processor_name, &name_len);
 
     int device_count = 0;
     CUDA_CHECK(cudaGetDeviceCount(&device_count));
@@ -41,19 +58,36 @@ int main(int argc, char** argv) {
     int device = rank % device_count;
     CUDA_CHECK(cudaSetDevice(device));
 
+    cudaDeviceProp prop{};
+    CUDA_CHECK(cudaGetDeviceProperties(&prop, device));
+
     constexpr int n = 4;
     double h[n] = {};
     double* d = nullptr;
 
     CUDA_CHECK(cudaMalloc(&d, n * sizeof(double)));
+    int launch_cpu = current_cpu();
     fill_rank<<<1, n>>>(d, n, rank);
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaDeviceSynchronize());
     CUDA_CHECK(cudaMemcpy(h, d, n * sizeof(double), cudaMemcpyDeviceToHost));
     CUDA_CHECK(cudaFree(d));
 
-    std::printf("cuda_mpi_smoke: rank %d/%d device=%d device_count=%d value=%.1f\n",
-                rank, size, device, device_count, h[0]);
+    std::printf(
+        "cuda_mpi_smoke: rank %d/%d host=%s pid=%d launch_cpu=%d "
+        "gpu_device=%d/%d gpu_name=\"%s\" gpu_pci=%04x:%02x:%02x value=%.1f\n",
+        rank,
+        size,
+        processor_name,
+        getpid(),
+        launch_cpu,
+        device,
+        device_count,
+        prop.name,
+        prop.pciDomainID,
+        prop.pciBusID,
+        prop.pciDeviceID,
+        h[0]);
 
     MPI_Finalize();
     return 0;
